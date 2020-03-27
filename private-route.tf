5327e825907a463cc9536140c1bb2f613cfdba66
naveen-19 <55230320+naveen-19@users.noreply.github.com>
2020-03-27 12:08:30 +0530

    Update private-route.tf

--- private-route.tf
+++ private-route.tf
@@ -1,6 +1,7 @@
 ## Enable internal route for NAT gw
 resource "aws_route" "nat_gtw" {
+ count = "${length(var.public_subnets)}"
  route_table_id = "${aws_route_table.eks-private.id}"
  destination_cidr_block = "0.0.0.0/0"
- nat_gateway_id = "${aws_nat_gateway.nat_gw.id}"    
+ nat_gateway_id = "${aws_nat_gateway.nat_gw.*.id[count.index]}"    
 }
